d9d4ef2aef987d3f92123c62a3ab70e488e9297d
Lisa Nishiyama <lisa.nishiyama@gmail.com>
2016-04-20 16:24:21 +0900

    Add mocha sample

---
+++ js/test/test-mocha-sample-module.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var myModule = require('../sandbox/mocha-sample-module');
+
+describe('myModule', function () {
+    describe('greet', function () {
+        it('引数に応じて決まった文字列を返すこと', function () {
+            assert.equal(myModule.greet('taro'), 'Hello,taro');
+        });
+    });
+});
