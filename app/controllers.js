b0c2c10826cb7794c6aaf9655b63c6b58ca2bcad
Lisa Nishiyama <lisa.nishiyama@gmail.com>
2016-05-08 01:09:10 +0900

    Render records in table

--- app/controllers.js
+++ app/controllers.js
@@ -8,6 +8,6 @@ recordControllers.controller('recordCtrl', ['$scope',　'$http',
   function($scope, $http) {
     $http.get('http://localhost:3000/db?name=%E5%B7%9D%E4%B8%AD%E8%8E%89%E7%B4%97&distance=100m&style=背泳ぎ')
       .success(function(data) {
-        $scope.list=data;
+        $scope.records=data;
       });
   }]);
