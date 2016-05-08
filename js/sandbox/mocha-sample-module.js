cf8f633026ffc1d5291b29a362b08d5a442a3f45
Lisa Nishiyama <lisa.nishiyama@gmail.com>
2016-05-09 00:00:45 +0900

    Delete unused sandbox js

--- js/sandbox/mocha-sample-module.js
+++
@@ -1,8 +0,0 @@
-exports.greet = function(name) {
-  return "Hello,"+ name;
-};
-
-exports.greetAsync = function(name,callback) {
-    var greet = "Hello,"+ name;
-    callback(greet);
-};
--- js/sandbox/test-let.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-  const STATIC_VAR = '定数';
-  let a = 'varは使わない';
-
-  for (let i = 0; i < 10; i++) {
-    console.log(a);
-  }
-}());
