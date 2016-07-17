3004e0d469ddd1cbbda15d167fc03844ad6f4cff
Lisa Nishiyama <lisa.nishiyama@gmail.com>
2016-07-17 17:46:33 +0900

    Update tdsystem parser to generate DB records in the current design

--- db/js/tdsystem/result-parser-2008.js
+++ db/js/tdsystem/result-parser-2008.js
@@ -3,40 +3,84 @@
  */
 'use strict';
 var resultParser = (function() {
-  const EVENT_PAT = /(男子|女子|混合)([^m]+m)(自由形|背泳ぎ|平泳ぎ|バタフライ|個人メドレー|フリーリレー|メドレーリレー)/;
-  const RESULT_PAT = /([0-9]+)　(.+).+\((.+)\)[^0-9:]+([0-9]*:*[0-9]{2}\.[0-9]{2})/;
-  const GRADE_PAT = /([小中高大][1-6１-６])/;
-  let parseDocument = function($) {
-    let i = 0;
+  const EVENT_PAT = /(男子|女子|混合)([^m]+)m(自由形|背泳ぎ|平泳ぎ|バタフライ|個人メドレー|フリーリレー|メドレーリレー)/;
+  const RELAY_PAT = /(男子|女子|混合)([^m]+)m(フリーリレー|メドレーリレー)/;
+  const AGE_PAT = /([0-9]+)[~・]([0-9]+)歳/;
+  const RESULT_PAT = /([0-9]+)(.+)\((.+)\)([0-9]*):*([0-9]*):*([0-9]{2}\.[0-9]{2})/;
+  const GRADE_PAT = /([小中高大][1-6])/;
+  let parseDocument = function(meetId, $) {
     let text = '';
     let ret = {};
+    let eventBase = {};
+    let currentEventId = -1;
+    ret.events = [];
+    ret.races = [];
+    ret.users = [];
+    ret.teams = [];
+    ret.user_team = [];
     ret.results = [];
+    ret.user_result = [];
 
     $('td').each(function(i) {
-      let text = $(this).find('font').text();
-      let eventMatch = EVENT_PAT.exec(text.replace(/[\s　]/g,''));
-      let resultMatch = RESULT_PAT.exec(text.trim());
-
-      if (i === 0) { // The 1st row include title, date, and venue
-        ret.meet = parseTitle(text);
-      } else if (eventMatch) {
-        ret.event = {};
-        ret.event.sex = eventMatch[1];
-        ret.event.distance = eventMatch[2];
-        ret.event.style = eventMatch[3];
+      let text = $(this).find('font').text().normalize('NFKC').replace(/[\s]/g,'');
+      let eventMatch = EVENT_PAT.exec(text);
+      let ageMatch = AGE_PAT.exec(text);
+      let resultMatch = RESULT_PAT.exec(text);
+      if (eventMatch) {
+        eventBase.sex = eventMatch[1];
+        eventBase.distance = parseInt(eventMatch[2], 10);
+        eventBase.style = eventMatch[3];
+        if (RELAY_PAT.exec(text)) {
+          eventBase.relay = true;
+        } else {
+          eventBase.relay = false;
+        }
+      } else if (ageMatch) {
+        let event = Object.assign({}, eventBase);
+        event.age = parseInt(ageMatch[1], 10);
+        ret.events.push(event);
+        currentEventId++;
+        ret.races.push({
+          meetId: meetId,
+          eventId: currentEventId
+        });
       } else if (resultMatch) {
-        let result = {};
+        let result = {
+          raceId: currentEventId
+        };
         let name = resultMatch[2].replace(/[\s　]/g,'').trim();
         let gradeMatch = GRADE_PAT.exec(name);
 
-        result.rank = resultMatch[1];
-        result.name = gradeMatch ? name.substr(0, gradeMatch.index) : name;
+        result.rank = parseInt(resultMatch[1], 10);
+        ret.users.push(gradeMatch ? name.substr(0, gradeMatch.index) : name);
+        let userId = ret.users.length - 1;  // Suppose no one can swim over twice in the same race
+
+        let grade;
         if (gradeMatch) {
-          result.grade = gradeMatch[1];
+          grade = gradeMatch[1];
         }
-        result.team = resultMatch[3].trim();
-        result.time = resultMatch[4];
+
+        let team = resultMatch[3].trim();
+        let index = ret.teams.indexOf(team);
+        if (index === -1) { // Does not exist
+          ret.teams.push(team);
+          index = ret.teams.length - 1;
+        }
+        ret.user_team.push({
+          userId: userId,
+          teamId: index
+        });
+
+        let hour = resultMatch[4];
+        let min = resultMatch[5];
+        let sec = resultMatch[6];
+        result.record = (hour && min ? hour + ' hour ' : '') + (hour && !min ? hour + ' minute ' : '') + sec + ' seconds';
         ret.results.push(result);
+
+        ret.user_result.push({
+          userId: userId,
+          resultId: ret.results.length - 1
+        });
       }
     });
 
--- db/test/unit/tdsystem/result-parser-2008.spec.js
+++ db/test/unit/tdsystem/result-parser-2008.spec.js
@@ -2,12 +2,42 @@
 var client = require('cheerio-httpcli');
 var parser = require('../../../js/tdsystem/result-parser-2008');
 var testUrl = 'http://www.tdsystem.co.jp/2016/201601/16CMC/017.HTM';
+var normalize = function(text) {
+  return text.normalize('NFKC');
+};
 describe('2016年大会結果パージングのテスト', function() {
-  client.fetch(testUrl, 'utf-8', function(err, $, res) {
-    let document = parser.parseDocument($);
-    console.log(document);　　
-    it('should return a right area value', function() {
-      expect(true).toBe(true);
+  it('should find a right result of a given swimmer name',
+    function(done) {
+      client.fetch(testUrl, 'utf-8', function(err, $, res) {
+          let parseResult = parser.parseDocument(0, $);
+          expect(parseResult).toBeDefined();
+
+          let users = parseResult.users;
+          expect(users).toBeDefined();
+
+          let results = parseResult.results;
+          expect(results).toBeDefined();
+
+          let userResult = parseResult.user_result;
+          expect(userResult).toBeDefined();
+
+          let userId = users.indexOf(normalize('伊藤勝介'));
+          expect(userId).toBeGreaterThan(-1);
+
+          let userResultElm = userResult.find(function(elm) {
+            expect(elm.userId).toBeDefined();
+            if (elm.userId === userId) {
+              return true;
+            }
+            return false;
+          });
+          expect(userResultElm).toBeDefined();
+
+          let result = results[userResultElm.resultId];
+          expect(result).toBeDefined();
+          expect(result.rank).toBe(5);
+          expect(result.record).toBe('46.20 seconds');
+          done();
+        });
     });
-  });
 });
