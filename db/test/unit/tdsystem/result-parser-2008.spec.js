9744dcee33eb9b3c17bf3acb30a1bd9d7c25523b
Lisa Nishiyama <lisa.nishiyama@gmail.com>
2016-09-11 18:45:17 +0900

    :green_heart: Update result-parse test case
    Follow the current parser output design.

--- db/test/unit/tdsystem/result-parser-2008.spec.js
+++ db/test/unit/tdsystem/result-parser-2008.spec.js
@@ -9,34 +9,21 @@ describe('2016年大会結果パージングのテスト', function() {
   it('should find a right result of a given swimmer name',
     function(done) {
       client.fetch(testUrl, 'utf-8', function(err, $, res) {
-          let parseResult = parser.parseDocument(0, $);
+          let parseResult = parser.parseDocument($);
           expect(parseResult).toBeDefined();
+          expect(parseResult.results).toBeDefined();
 
-          let users = parseResult.users;
-          expect(users).toBeDefined();
-
-          let results = parseResult.results;
-          expect(results).toBeDefined();
-
-          let userResult = parseResult.user_result;
-          expect(userResult).toBeDefined();
-
-          let userId = users.indexOf(normalize('伊藤勝介'));
-          expect(userId).toBeGreaterThan(-1);
-
-          let userResultElm = userResult.find(function(elm) {
-            expect(elm.userId).toBeDefined();
-            if (elm.userId === userId) {
+          let userName = normalize('伊藤勝介');
+          let resultElm = parseResult.results.find(function(elm) {
+            expect(elm.user).toBeDefined();
+            if (elm.user === userName) {
               return true;
             }
             return false;
           });
-          expect(userResultElm).toBeDefined();
-
-          let result = results[userResultElm.resultId];
-          expect(result).toBeDefined();
-          expect(result.rank).toBe(5);
-          expect(result.record).toBe('46.20 seconds');
+          expect(resultElm).toBeDefined();
+          expect(resultElm.rank).toBe(5);
+          expect(resultElm.record).toBe('46.20 seconds');
           done();
         });
     });
