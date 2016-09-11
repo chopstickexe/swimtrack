00858f65e408b8199348d0e84e51f82234d7466d
Lisa Nishiyama <lisa.nishiyama@gmail.com>
2016-09-11 17:05:23 +0900

    :shower: Delete unused js

--- js/sandbox/crawl-tdsystem.js
+++
@@ -1,4 +0,0 @@
-var crawler = require('../crawler.js');
-var parser = require('../tdsystem/result-parser.js');
-var tdSystemCrawler = crawler(parser, { targetDomain: 'www.tdsystem.co.jp'});
-tdSystemCrawler.download('http://www.tdsystem.co.jp/2015/201509/23RUNEM/PRO.HTM', 0);
