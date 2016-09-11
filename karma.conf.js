ec6961f14de6105071afd8369d598a70fdb61e35
Lisa Nishiyama <lisa.nishiyama@gmail.com>
2016-09-11 18:27:52 +0900

    :shower: Delete unused unit test

--- karma.conf.js
+++ karma.conf.js
@@ -7,9 +7,7 @@ module.exports = function(config) {
     files: [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'js/sandbox/circle.js',
-      'test/unit/*.js'
+      'app/bower_components/angular-mocks/angular-mocks.js'
     ],
 
     autoWatch: true,
--- test/unit/circle-test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var circle = require('../../js/sandbox/circle');
-describe('area 関数のテスト', function() {
-  it('should return a right area value', function() {
-    expect(circle.area(1)).toBe(Math.PI);
-  });
-});
