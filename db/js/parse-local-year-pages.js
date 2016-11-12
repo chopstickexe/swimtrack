af016546ce5a24a1ebbed94636e19a7ae223a98a
Lisa Nishiyama <lisa.nishiyama@gmail.com>
2016-11-12 18:21:31 +0900

    Parse meets and venues

--- db/js/parse-local-year-pages.js
+++ db/js/parse-local-year-pages.js
@@ -9,61 +9,89 @@
 
   var getId = function(db, selectStatement, insertStatement, args) {
     return new Promise(function(resolve, reject) {
-      db.oneOrNone(selectStatement, args)
-        .then(function(row) {
-          if(!row) {
-            db.one(insertStatement, args)
-              .then(function(row) {
-                return resolve(row.id);
-              });
-          } else {
-            return resolve(row.id);
-          }
+      db.task(function(t) {
+          t.oneOrNone(insertStatement, args)
+            .then(function(row) {
+              console.log('row=' + row);
+            })
+            .catch(function(err) {
+              console.log(err);
+            });
         })
         .catch(function(err) {
-          db.oneOrNone(selectStatement, args)
-            .then(function(row) { // just conflict to another insert process
-              if (row) {
-                return resolve(row.id);
-              } else {
-                return reject(err);
-              }
-            });
-          });
+          return reject(err);
         });
-    };
+    });
+  };
 
-  /**
-   * Store venue
-   */
+  var initVenues = function(db) {
+    return new Promise(function(resolve, reject) {
+      db.any('SELECT * FROM venues')
+        .then(function(rows) {
+          let ret = {};
+          ret.map = {};
+          ret.maxId = -1;
+          for (const row of rows) {
+            ret.map[row.name] = row;
+            if (ret.maxId < row.id) {
+              ret.maxId = row.id;
+            }
+          }
+          return resolve(ret);
+        })
+        .catch(function(err) {
+          console.error('Error in initVenues');
+          return reject(err);
+        });
+    });
+  };
+
+  var initMeets = function(db) {
+    return new Promise(function(resolve, reject) {
+      db.any('SELECT * FROM meets')
+        .then(function(rows) {
+          let ret = {};
+          ret.map = {};
+          ret.maxId = -1;
+          for (const row of rows) {
+            ret[row.name] = row;
+            if (ret.maxId < row.id) {
+              ret.maxId = row.id;
+            }
+          }
+          return resolve(ret);
+        })
+        .catch(function(err) {
+          console.error('Error in initMeets');
+          return reject(err);
+        });
+    });
+  };
+
+
   var getVenueId = function(db, venue) {
     return getId(db, 'SELECT id FROM venues WHERE name = $1 AND city = $2',
-      'INSERT INTO venues(name, city) VALUES($1, $2) RETURNING id',
-      [venue.name, venue.city]);
+      'INSERT INTO venues(name, city) VALUES($1, $2) ON CONFLICT(name, city) DO NOTHING RETURNING id', [venue.name, venue.city]);
   };
 
   var getMeetId = function(db, venueId, meet) {
     return getId(db, 'SELECT id FROM meets WHERE name = $1 AND start_date = $2 AND venue_id = $4',
-      'INSERT INTO meets(name, start_date, dates, venue_id, course) SELECT $1, $2, CAST($3 AS DATE[]), $4, $5 RETURNING id',
-      [meet.name, meet.days[0], meet.days, venueId, meet.venue.course]);
+      'INSERT INTO meets(name, start_date, dates, venue_id, course) SELECT $1, $2, CAST($3 AS DATE[]), $4, $5 RETURNING id', [meet.name, meet.days[0], meet.days, venueId, meet.venue.course]);
   };
 
   var getEventId = function(db, event) {
     return getId(db, 'SELECT id FROM events WHERE sex = $1 AND distance = $2 AND style = $3 AND age = $4 AND relay = $5',
-      'INSERT INTO events(sex, distance, style, age, relay) VALUES($1, $2, $3, $4, $5) RETURNING id',
-      [event.sex, event.distance, event.style, event.age, event.relay]);
+      'INSERT INTO events(sex, distance, style, age, relay) VALUES($1, $2, $3, $4, $5) RETURNING id', [event.sex, event.distance, event.style, event.age, event.relay]);
   };
 
   var getRaceId = function(db, meetId, eventId) {
     return getId(db, 'SELECT id FROM races WHERE meet_id = $1 AND event_id = $2',
-      'INSERT INTO races(meet_id, event_id) VALUES($1, $2) RETURNING id',
-      [meetId, eventId]);
+      'INSERT INTO races(meet_id, event_id) VALUES($1, $2) RETURNING id', [meetId, eventId]);
   };
 
   var getResultId = function(db, raceId, result) {
     return getId(db, 'SELECT id FROM results WHERE race_id = $1 AND rank = $2 AND record = $3',
-      'INSERT INTO results(race_id, rank, record) SELECT $1, $2, $3 RETURNING id',
-      [raceId, result.rank, result.record]);
+      'INSERT INTO results(race_id, rank, record) SELECT $1, $2, $3 RETURNING id', [raceId, result.rank, result.record]);
   };
 
   var getUserId = function(db, user) {
@@ -80,8 +108,7 @@
 
   var insertUserResult = function(db, userId, resultId) {
     return new Promise(function(resolve, reject) {
-      db.none('INSERT INTO user_result(user_id, result_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT * FROM user_result WHERE user_id = $1 AND result_id = $2)',
-        [userId, resultId])
+      db.none('INSERT INTO user_result(user_id, result_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT * FROM user_result WHERE user_id = $1 AND result_id = $2)', [userId, resultId])
         .then(function() {
           return resolve('done');
         })
@@ -93,8 +120,7 @@
 
   var insertUserTeamMeet = function(db, userId, teamId, meetId) {
     return new Promise(function(resolve, reject) {
-      db.none('INSERT INTO user_team_meet(user_id, team_id, meet_id) SELECT $1, $2, $3 WHERE NOT EXISTS (SELECT * FROM user_team_meet WHERE user_id = $1 AND team_id = $2 AND meet_id = $3)',
-        [userId, teamId, meetId])
+      db.none('INSERT INTO user_team_meet(user_id, team_id, meet_id) SELECT $1, $2, $3 WHERE NOT EXISTS (SELECT * FROM user_team_meet WHERE user_id = $1 AND team_id = $2 AND meet_id = $3)', [userId, teamId, meetId])
         .then(function() {
           return resolve('done');
         })
@@ -125,75 +151,79 @@
   /**
    * Main process
    */
-  const YEAR_TOP_PAGES = [
-    {
-      year: 2016,
-      url: 'http://www.tdsystem.co.jp/i2016.htm',
-      path: 'www.tdsystem.co.jp/i2016.htm'
-    },
-    {
-      year: 2015,
-      url: 'http://www.tdsystem.co.jp/i2015.htm',
-      path: 'www.tdsystem.co.jp/i2015.htm'
-    },
-    {
-      year: 2014,
-      url: 'http://www.tdsystem.co.jp/i2014.htm',
-      path: 'www.tdsystem.co.jp/i2014.htm'
-    },
-    {
-      year: 2013,
-      url: 'http://www.tdsystem.co.jp/i2013.htm',
-      path: 'www.tdsystem.co.jp/i2013.htm'
-    },
-    {
-      year: 2012,
-      url: 'http://www.tdsystem.co.jp/i2012.htm',
-      path: 'www.tdsystem.co.jp/i2012.htm'
-    }
-  ];
-  let db = pgp(process.env.DATABASE_URL);
-  let meetId;
-  for (const yearIndex in YEAR_TOP_PAGES) {
-    const yearTopPage = YEAR_TOP_PAGES[yearIndex];
-    console.log('Parse ' + yearTopPage.year);
-    let $ = util.parseLocalHtml(yearTopPage.path);
-    let yearParseResult = yearParser.parsePage(yearTopPage.year, $);
-    yearParseResult.meets.forEach(function(meet) {
-      getVenueId(db, meet.venue)
-        .then(function(venueId) {
-          getMeetId(db, venueId, meet)
-            .then(function(meetId) {
-              let url = meet.url;
-              if (!url) return;
-              //
-              // Parse meet page (PRO.HTM)
-              //
-              let meetPath = yearTopPage.path.substring(0, yearTopPage.path.lastIndexOf('/') + 1) + url;
-              let $meet = util.parseLocalHtml(meetPath);
-              let meetParseResult = meetParser.parsePage($meet);
-              meetParseResult.races.forEach(function(race) {
-                getEventId(db, race)
-                  .then(function(eventId) {
-                    getRaceId(db, meetId, eventId)
-                      .then(function(raceId) {
-                        processRace(db, meetId, raceId, meetPath.substring(0, meetPath.lastIndexOf('/') + 1) + race.page);
-                      })
-                      .catch(function(err) {
-                        console.error(err);
-                      });
-                  })
-                  .catch(function(err) {
-                    console.error(err);
-                  });
-              });
-            })
-            .catch(function(err) {
-              console.error(err);
-            });
-        }).catch(function(err){
-          console.error(err);
-        });
+  const YEAR_TOP_PAGES = [{
+    year: 2016,
+    url: 'http://www.tdsystem.co.jp/i2016.htm',
+    path: 'www.tdsystem.co.jp/i2016.htm'
+  }, {
+    year: 2015,
+    url: 'http://www.tdsystem.co.jp/i2015.htm',
+    path: 'www.tdsystem.co.jp/i2015.htm'
+  }, {
+    year: 2014,
+    url: 'http://www.tdsystem.co.jp/i2014.htm',
+    path: 'www.tdsystem.co.jp/i2014.htm'
+  }, {
+    year: 2013,
+    url: 'http://www.tdsystem.co.jp/i2013.htm',
+    path: 'www.tdsystem.co.jp/i2013.htm'
+  }, {
+    year: 2012,
+    url: 'http://www.tdsystem.co.jp/i2012.htm',
+    path: 'www.tdsystem.co.jp/i2012.htm'
+  }];
+  let db = pgp({
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD
+  });
+  Promise.all([initVenues(db), initMeets(db)])
+    .then(function(values) {
+      let venues = values[0].map;
+      let venueMaxId = values[0].maxId;
+      let meets = values[1].map;
+      let meetMaxId = values[1].maxId;
+      //
+      // Process each year index page
+      //
+      for (const yearIndex in YEAR_TOP_PAGES) {
+        const yearTopPage = YEAR_TOP_PAGES[yearIndex];
+        console.log('Parse ' + yearTopPage.year);
+        let $ = util.parseLocalHtml(yearTopPage.path);
+        let yearParseResult = yearParser.parsePage(yearTopPage.year, $);
+        let meets = yearParseResult.meets;
+        if (!meets) {
+          console.log('No meet found.');
+          continue;
+        }
+        console.log(meets.length + ' meets found.');
+        for (let meet of meets) {
+          if (!meet.name || meets[meet.name]) { // No name or already in DB
+            continue;
+          }
+          //
+          // Define meet id
+          //
+          meet.id = ++meetMaxId;
+          //
+          // Define venue id
+          //
+          if (!meet.venue) {
+            console.log('Skipped by invalid venue info: name = ' + meet.name);
+            continue;
+          }
+          if (venues[meet.venue.name]) {
+            meet.venue.id = venues[meet.venue.name].id;
+          } else {
+            meet.venue.id = ++venueMaxId;
+          }
+        }
+      }
+    })
+    .catch(function(err) {
+      console.error('Error in main');
+      console.error(err);
     });
-  }
 }());
