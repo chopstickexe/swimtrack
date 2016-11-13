764accef5a2b91ac484972421fc5485108be7136
Lisa Nishiyama <lisa.nishiyama@gmail.com>
2016-11-13 17:14:28 +0900

    Set record ids out of Postgres

--- db/sql/create-tables.sql
+++ db/sql/create-tables.sql
@@ -2,14 +2,14 @@ CREATE TYPE course_type AS ENUM ('長水路', '短水路');
 CREATE TYPE sex_type AS ENUM ('男子', '女子', '混合');
 
 CREATE TABLE venues(
-  id   SERIAL PRIMARY KEY,
+  id   INTEGER PRIMARY KEY,
   name VARCHAR(50),
   city VARCHAR(50),
   UNIQUE(name, city)
 );
 
 CREATE TABLE meets(
-  id         SERIAL PRIMARY KEY,
+  id         INTEGER PRIMARY KEY,
   name       VARCHAR(100),
   start_date DATE,
   dates      DATE[],
@@ -19,7 +19,7 @@ CREATE TABLE meets(
 );
 
 CREATE TABLE events(
-  id       SERIAL PRIMARY KEY,
+  id       INTEGER PRIMARY KEY,
   sex      sex_type,
   distance INTEGER,
   style    VARCHAR(10),
@@ -35,14 +35,14 @@ CREATE INDEX distance_on_events ON events(distance);
 CREATE INDEX style_on_events ON events(style);
 
 CREATE TABLE races(
-  id         SERIAL PRIMARY KEY,
+  id         INTEGER PRIMARY KEY,
   meet_id    INTEGER REFERENCES meets(id),
   event_id   INTEGER REFERENCES events(id),
   UNIQUE(meet_id, event_id)
 );
 
 CREATE TABLE users(
-  id   SERIAL PRIMARY KEY,
+  id   INTEGER PRIMARY KEY,
   name VARCHAR(30),
   UNIQUE(name)
 );
@@ -50,7 +50,7 @@ CREATE TABLE users(
 CREATE INDEX name_on_users ON users(name);
 
 CREATE TABLE teams(
-  id   SERIAL PRIMARY KEY,
+  id   INTEGER PRIMARY KEY,
   name VARCHAR(30),
   UNIQUE(name)
 );
@@ -63,7 +63,7 @@ CREATE TABLE user_team_meet(
 );
 
 CREATE TABLE results(
-  id       SERIAL PRIMARY KEY,
+  id       INTEGER PRIMARY KEY,
   race_id  INTEGER REFERENCES races(id),
   rank     INTEGER,
   record   INTERVAL HOUR TO SECOND NOT NULL,
